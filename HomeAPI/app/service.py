ae05ee3e8067306040fd9ff9f761ec2eedb94b63
Ivanchik <you@example.com>
2026-07-16 15:17:41 +1000

    json -> sqlite

--- HomeAPI/app/service.py
+++ HomeAPI/app/service.py
@@ -3,25 +3,66 @@ from fastapi import HTTPException
 import json
 from pydantic import BaseModel
 from starlette import status
+import sqlite3
+from pathlib import Path
 
 from HomeAPI.app.const import Category
 from HomeAPI.app.schema import TaskCreate
 
-with open('HomeAPI/app/db.json', 'r', encoding='utf-8') as f:
-    tasks = json.load(f)
+from pathlib import Path
+import sqlite3
 
-def find_task(task_id:int) -> Dict[str, Any]:
+DB_PATH = Path(__file__).parent / 'task_db.db'
+db = sqlite3.connect(DB_PATH)
+cursor = sqlite3.Cursor(db)
+
+cursor.execute('SELECT * FROM tasks;')
+rows = cursor.fetchall()
+tasks = []
+for row in rows:
+    tasks.append(
+        {'id': row[0],
+         'title': row[1],
+         'description': row[2],
+         'done': row[3],
+         'subject': row[4],
+         'priority': row[5],
+         'category': row[6]}
+    )
+db.commit()
+db.close()
+
+# DB_PATH = Path(__file__).parent / 'db.json'
+#
+# with open(DB_PATH, 'r', encoding='utf-8') as f:
+#     tasks = json.load(f)
+
+def find_task(task_id: int) -> Dict[str, Any]:
     for task in tasks:
         if task["id"] == task_id:
             return task
     raise HTTPException(status_code=404, detail='Task id not found')
 
+
 def patch_dump(task: Dict) -> None:
     for t in tasks:
-        if t["id"] == task["id"]:
+        if t['id'] == task['id']:
             tasks[tasks.index(t)] = task
-            with open('HomeAPI/app/db.json', 'w', encoding='utf-8') as f:
-                json.dump(tasks, f, ensure_ascii=False, indent=2)
+
+    db = sqlite3.connect(DB_PATH)
+    cursor = sqlite3.Cursor(db)
+    cursor.execute(f"""UPDATE tasks 
+    SET id = {task['id']},
+    title = '{task['title']}',
+    description = '{task['description']}',
+    done = '{task['done']}',
+    subject = '{task['subject']}',
+    priority = {task['priority']},
+    category = '{task['category']}'
+    WHERE id = {task['id']}""")
+    db.commit()
+    db.close()
+
 
 def get_tasks_by_query(
         done: bool | None = None,
@@ -42,6 +83,7 @@ def get_tasks_by_query(
         new_tasks = [task for task in new_tasks if task["category"].lower() == category.lower()]
     return new_tasks[:limit]
 
+
 def task_update_by_schema(task_id: int, task_update: BaseModel) -> None:
     task = find_task(task_id=task_id)
     update_data = task_update.model_dump(exclude_unset=True)
@@ -49,21 +91,37 @@ def task_update_by_schema(task_id: int, task_update: BaseModel) -> None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail='Нет данных для обновления',
-            )
+        )
     task.update(update_data)
     patch_dump(task=task)
 
+
 def post_task(task: TaskCreate) -> Dict[str, Any]:
     global tasks
+    db = sqlite3.connect(DB_PATH)
+    cursor = sqlite3.Cursor(db)
     new_id = tasks[-1]['id']
     new_task = {"id": new_id + 1, **task.model_dump()}
     tasks.append(new_task)
-    with open('HomeAPI/app/db.json', 'w', encoding='utf-8') as f:
-        json.dump(tasks, f, ensure_ascii=False, indent=2)
+    cursor.execute(f"""INSERT INTO tasks
+    VALUES({new_task['id']},
+    '{new_task['title']}',
+    '{new_task['description']}',
+    '{new_task['done']}',
+    '{new_task['subject']}',
+     {new_task['priority']},
+    '{new_task['category']}')""")
+    db.commit()
+    db.close()
     return new_task
+
 
 def delete_task_from_json(task_id: int) -> None:
     task = find_task(task_id=task_id)
     tasks.remove(task)
-    with open('HomeAPI/app/db.json', 'w', encoding='utf-8') as f:
-        json.dump(tasks, f, ensure_ascii=False, indent=2)
+
+    db = sqlite3.connect(DB_PATH)
+    cursor = sqlite3.Cursor(db)
+    cursor.execute(f"DELETE FROM tasks WHERE id = {task['id']}")
+    db.commit()
+    db.close()
